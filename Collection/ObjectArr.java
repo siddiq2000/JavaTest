0399c5922c3f6b05e5db984dcd660fdb87efb5e8
siddiq2000 <siddiq2000.sm@gmail.com>
2024-02-10 23:52:12 +0530

    Added Coolection w/ 1 program

---
+++ Collection/ObjectArr.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class ObjectArr {
+    public static void main(String[] args) {
+        Object obj[] = new Object[5];
+        obj[0] = 123;
+        obj[1] = "Sid";
+        obj[2] = 3.14f;
+        obj[3] = false;
+        obj[4] = new java.util.Date();
+        for(int i=0; i<obj.length; i++) {
+            System.out.println(obj[i]);
+        }
+    }
+}
